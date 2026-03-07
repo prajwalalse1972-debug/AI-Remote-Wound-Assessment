f3e3cc7a4724b505cd5b4415d6b9e912cf8ea366
Prajwal Alse <244651774+prajwalalse1972-debug@users.noreply.github.com>
2026-03-07 18:44:02 +0530

    Day01_code

---
+++ Day01/day01_code.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("WoundAI - Remote Wound Assessment")
+st.write("Hello! This app will help doctors assess wounds remotely.")
+st.write("Built for Biobytes Hackathon 2026 🏆")
